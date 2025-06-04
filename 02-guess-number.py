533f49de202a32b50479bb0383f60aa0641fed04
JeanCarlos <jmatochepascual@gmail.com>
2025-06-04 09:06:02 -0500

    feat: :sparkles: Add guess number game

---
+++ 02-guess-number.py
@@ -0,0 +1,32 @@
+# Guess the number
+import random
+
+random_number = random.randint(1, 100)
+oportunity = 8
+attempts = 0
+
+nombre = input("Enter your name: ")
+print(f"""
+==== GUESS THE NUMBER ====
+
+Hello {nombre},
+You have {oportunity} opportunities to guess the number
+
+==== GUESS THE NUMBER ====
+""")
+
+for i in range(oportunity):
+    numero = int(input("Enter a number between 1 and 100: "))
+    attempts += 1
+    if numero < 1 or numero > 100:
+        print("\nInvalid input, please enter a number between 1 and 100")
+        continue
+    elif numero == random_number:
+        print(f"\nCongratulations {nombre}, you guessed the number! in {attempts} attempts")
+        exit()
+    elif numero < random_number:
+        print("\nToo low, try again")
+    else:
+        print("\nToo high, try again")
+
+print(f"\n:c {nombre}, you lost the game")
