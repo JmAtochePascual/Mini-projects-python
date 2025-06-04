07e35de793c9fbaa0d8a24d1f0a3c182dd27bd7a
JeanCarlos <jmatochepascual@gmail.com>
2025-06-04 09:05:38 -0500

    feat: :recycle: Update text-analyzer

---
+++ 01-text-analyzer.py
@@ -0,0 +1,30 @@
+print("""
+==== ANALIZER TEXT ====
+
+Enter a text long or short: Example: paragraph, text, poem, article, etc.
+
+also, you need to enter three letters to analyze: Example: a, e, i, etc.
+
+==== ANALYZER TEXT ====
+\n""")
+
+text = input("\nEnter a text: ")
+
+first_letter = input("\nEnter the first letter: ")
+second_letter = input("\nEnter the second letter: ")
+third_letter = input("\nEnter the third letter: ")
+
+paragraphs = text.split(" ")
+paragraph_secret = "python"
+
+print("\n\n === RESULTS ===\n")
+
+print("\nThe letter 'a' appears: ", text.lower().count(first_letter), "times")
+print("\nThe letter 'e' appears: ", text.lower().count(second_letter), "times")
+print("\nThe letter 'i' appears: ", text.lower().count(third_letter), "times")
+
+print("\nThe text has: ", len(paragraphs), "paragraphs")
+print("\nThe first letter is: ", text[0])
+print("\nThe last letter is: ", text[-1])
+print("\nThe reversed text is: ", text[::-1])
+print("\nIs the word \"python\" in the text?: ", paragraph_secret in text.lower())
